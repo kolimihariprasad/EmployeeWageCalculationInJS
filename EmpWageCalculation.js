bfff2fce9438a25c6d96768ff0adf2bd8c790153
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-12-02 20:09:56 +0530

    Array function implementation Using Arrow function

--- EmpWageCalculation.js
+++ EmpWageCalculation.js
@@ -43,3 +43,93 @@ while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
     })
 }
 console.log("UC10: "+ empDailyHrsAndWageArr)
+
+//UC11
+let totalWages = empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage >0)
+                    .reduce((totalWage, dailyHrsAndWage) => totalWage += dailyHrsAndWage.dailyWage,0);
+
+let totalHrs = empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage > 0)
+                    .reduce((totalHrs, dailyHrsAndWage) => totalHrs += dailyHrsAndWage.dailyWage,0);
+console.log("UC 11A Total Hours: "+totalHrs+" Total Wages: "+totalWages)
+process.stdout.write("UC11B Full Working Days")
+empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyHrsAndWage.dailyHrs == 8)
+                        .forEach(dailyHrsAndWage => process.stdout.write(dailyHrsAndWage.toString()))
+
+let partWrkDayStrArr = empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyHrsAndWage.dailyHrs == 4)
+                        .map(dailyHrsAndWage => dailyHrsAndWage.toString())
+console.log("\nUC11C PartTime Working Day String: "+partWrkDayStrArr)
+
+let nonWorkingDayNums = empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyHrsAndWage.dailyHrs == 0)
+                            .map(dailyHrsAndWage => dailyHrsAndWage.dayNum);
+console.log("UC 11D Non Working Days: "+nonWorkingDayNums)
+
+let isAllFullTimeWage = empDailyHrsAndWageArr.every(dailyHrsAndWage => dailyHrsAndWage.dailyHrs > 0)
+console.log("UC 11E is all Wages is Full Time: "+isAllFullTimeWage)
+
+let isAnyPartimeWage = empDailyHrsAndWageArr.some(dailyHrsAndWage => dailyHrsAndWage.dailyHrs == 4)
+console.log("UC 11F is there any partime Time Days: "+isAnyPartimeWage)
+
+let NumWrkingDays = empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyHrsAndWage.dailyHrs > 0)
+                        .map(dailyHrsAndWage => dailyHrsAndWage.dayNum.toString())
+console.log("UC 11G is all Wages is Full Time: "+NumWrkingDays)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
