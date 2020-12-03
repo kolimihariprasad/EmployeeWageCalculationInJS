cc13844a2b26d9f048b55be854dfe428e8d9a9c0
kolimihariprasad <kolimi.hariprasad@gmail.com>
2020-12-03 20:31:04 +0530

    added regex validation

--- EmployeePayrol.js
+++ EmployeePayrol.js
@@ -1,8 +1,5 @@
 class EmpPayrollData {
-    id;
-    salary;
-    gender;
-    startDate;
+    
     constructor(...params) {
         this.id =  params[0];
         this.name = params[1];
@@ -18,6 +15,37 @@ class EmpPayrollData {
         } else throw "Invalid Name"
     }
 
+    get id() { return this._id; }
+    set id(id) { 
+        let idRegex = RegExp("^[1-9]{1}[0-9]*$")
+        if(idRegex.test(id)){
+            this._id = id;
+        } else throw "Invalid id"
+    }
+
+    get salary() { return this._salary; }
+    set salary(salary) { 
+        let sRegex = RegExp("^[1-9]{1}[0-9]*$")
+        if(sRegex.test(salary)){
+            this._salary = salary;
+        } else throw "Invalid salary"
+    }
+    get gender() { return this._gender; }
+    set gender(gender) { 
+        let gRegex = RegExp("^[MF]$")
+        if(gRegex.test(gender)){
+            this._gender = gender;
+        } else throw "Invalid gender"
+    }
+    
+    get startDate() { return this._startDate; }
+    set startDate(startDate) { 
+        let date = new Date()
+        if(startDate < date){
+            this._startDate = startDate;
+        } else throw "Invalid Date"
+    }
+
     toString() {
         const options = {year: 'numeric', month: 'long', day:'numeric'};
         const empDate = this.startDate === undefined ? "undefined" :
@@ -26,13 +54,13 @@ class EmpPayrollData {
                 +", startDate: "+empDate
     }
 }
-let empPayrollData = new EmpPayrollData(1, "Sami", 35000);
-console.log(empPayrollData.toString());
+//let empPayrollData = new EmpPayrollData(1, "Sami", 35000);
+//console.log(empPayrollData.toString());
 try {
-empPayrollData.name = "john";
-console.log(empPayrollData.toString());
+    //empPayrollData.name = "john";
+    //console.log(empPayrollData.toString());
+    let newempPayrollData = new EmpPayrollData(1, "Mike", 35000,"M", new Date());
+    console.log(newempPayrollData.toString());
 } catch(e) {
     console.error(e)
 }
-let newempPayrollData = new EmpPayrollData(1, "Mike", 35000,"F", new Date());
-console.log(newempPayrollData.toString());
